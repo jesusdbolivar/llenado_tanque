bc376fe9d3efc09e853d2aa59d0031619097d9d8
Jesus Bolivar <bolijesus98@gmail.com>
2023-06-09 21:56:20 -0500

    Llenado con grifo rojo automatico

--- sim.py
+++ sim.py
@@ -15,6 +15,7 @@ pygame.display.set_caption("Juego del llenado y vaciado de un tanque rectangular
 # Colores
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
+RED = (255, 0, 0)
 
 # Coordenadas del tanque
 tank_x = width // 4
@@ -25,10 +26,16 @@ tank_width = width // 2
 tank_height = height // 2
 
 # Nivel de líquido actual (porcentaje)
-liquid_level = 50
+liquid_level = 0
 
-# Velocidad de cambio del nivel de líquido
-fill_speed = 5
+# Velocidad de llenado del tanque (porcentaje por segundo)
+fill_speed = 10
+
+# Velocidad de vaciado del tanque (porcentaje por segundo)
+empty_speed = 5
+
+# Tamaño del grifo de salida (porcentaje del ancho del tanque)
+grifo_size = 20
 
 # Bucle principal del juego
 running = True
@@ -37,27 +44,31 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                liquid_level += fill_speed
-                if liquid_level > 100:
-                    liquid_level = 100
-            elif event.key == pygame.K_DOWN:
-                liquid_level -= fill_speed
-                if liquid_level < 0:
-                    liquid_level = 0
+
+    # Incrementar el nivel del líquido automáticamente
+    if liquid_level < 100:
+        liquid_level += fill_speed * 0.01  # 0.01 para ajustar el porcentaje a la escala de 0 a 1
+        liquid_level = min(100, liquid_level)  # Limitar el nivel de líquido a 100
+
+    # Decrementar el nivel del líquido si el tanque no está vacío
+    if liquid_level > 0:
+        empty_rate = empty_speed * grifo_size / 100  # Cálculo de la velocidad de vaciado en base al tamaño del grifo
+        liquid_level -= empty_rate * 0.01  # 0.01 para ajustar el porcentaje a la escala de 0 a 1
+        liquid_level = max(0, liquid_level)  # Limitar el nivel de líquido a 0
 
     # Renderizar elementos gráficos
     screen.fill(WHITE)
 
-    # Calcular la altura del líquido en función del nivel actual
-    liquid_height = tank_height * liquid_level / 100
-
     # Dibujar el tanque
-    pygame.draw.rect(screen, BLUE, (tank_x, tank_y + tank_height - liquid_height, tank_width, liquid_height))
+    pygame.draw.rect(screen, BLUE, (tank_x, tank_y + tank_height - tank_height * liquid_level / 100, tank_width, tank_height * liquid_level / 100))
 
     # Dibujar el contorno del tanque
     pygame.draw.rect(screen, BLUE, (tank_x, tank_y, tank_width, tank_height), 2)
+
+    # Dibujar el grifo de salida
+    grifo_width = tank_width * grifo_size / 100
+    grifo_x = tank_x + (tank_width - grifo_width) / 2
+    pygame.draw.rect(screen, RED, (grifo_x, tank_y, grifo_width, 10))
 
     # Actualizar la pantalla
     pygame.display.flip()
